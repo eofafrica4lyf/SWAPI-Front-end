4803f2a8af0a465b8550e7c5d381d42208a7f2e6
Emmanuel Aboderin <aboderinemmanuel@gmail.com>
2019-09-02 00:36:07 +0100

    add hero banner, verbiage and search form as header.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,18 @@
 <template>
-  <h1>Header</h1>
+  <div class="header">
+    <h1>Header</h1>
+  </div>
 </template>
 
 <script>
+
 export default {
-  name: 'Header'
-}
+  name: "Header"
+};
 </script>
+
+<style scoped>
+.header{
+  background-image: url('../assets/images/hero-banner.jpg');
+}
+</style>
